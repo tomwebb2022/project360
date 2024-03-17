624ab6a33014bb2bb9803fcd6fd46d717e4750e8
Tom Webb <tomwebb1992@gmail.com>
2024-03-17 18:29:11 +0000

    created extra components

--- project360/src/App.jsx
+++ project360/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 // import examplegirl from './assets/examplegirl.jpg'
 import BackgroundVideo from './components/BackgroundVideo'
 import WhatIs from './components/WhatIs'
+import ImpactStatement from './components/ImpactStatement'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,11 +13,8 @@ function App() {
   <BackgroundVideo />  
 
   <WhatIs />
+<ImpactStatement />
 
-    <section className="impact-statement">
-      <h2>Impact Statement</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
-    </section>
         <h1>
           {/* contact details will go here  */}
         </h1>
