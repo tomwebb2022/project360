190e0db32c93b95f9e455aaebf88a94075015b20
Tom Webb <tomwebb1992@gmail.com>
2024-03-18 12:25:59 +0000

    corrected calendly scroll bar

--- project360/src/components/CalendlyWidget.jsx
+++ project360/src/components/CalendlyWidget.jsx
@@ -3,11 +3,6 @@ import { InlineWidget } from "react-calendly";
 const CalendlyWidget = () => {
   return (
 
-//     <div className="calendly"
-// data-url="https://calendly.com/anastasiaadamoudi/30min?month=2024-03"
-// style="min-width:320px;height:750px;">
-
-// </div>
 
     <div className="calendly">
  
