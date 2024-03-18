3daad02bfda8e8069513d9cc7e755dda2b150286
Tom Webb <tomwebb1992@gmail.com>
2024-03-18 11:21:28 +0000

    removing scroll bar from calendly

--- project360/src/components/CalendlyWidget.jsx
+++ project360/src/components/CalendlyWidget.jsx
@@ -2,11 +2,16 @@ import { InlineWidget } from "react-calendly";
 
 const CalendlyWidget = () => {
   return (
-    <div className="calendly"
-    style={{
-        height: '250px'
-    }}>
-      <InlineWidget url="https://calendly.com/anastasiaadamoudi/" />
+
+//     <div className="calendly"
+// data-url="https://calendly.com/anastasiaadamoudi/30min?month=2024-03"
+// style="min-width:320px;height:750px;">
+
+// </div>
+
+    <div className="calendly">
+ 
+      <InlineWidget url="https://calendly.com/anastasiaadamoudi/30min?month=2024-03"/>
     </div>
   );
 };
