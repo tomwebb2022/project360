aded40a520afdd2f7cc99f01de19d7091ebcf925
Tom Webb <tomwebb1992@gmail.com>
2024-03-20 13:13:09 +0000

    debugging iOS website issues

--- project360/src/components/BackgroundVideo.jsx
+++ project360/src/components/BackgroundVideo.jsx
@@ -4,7 +4,7 @@ function BackgroundVideo() {
   return (
     <div className="background-image">
       <h1 className="title">ATEDO 360 </h1>
-      <video playsInline autoPlay muted> 
+      <video playsInline autoPlay muted loop> 
       {/* added 'playsinline' accoring to chatgpt for ios */}
         <source src={dance} type="video/mp4" />
       </video>
