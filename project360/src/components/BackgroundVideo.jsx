2f6914e827b6d905398d1125f9e5e3eb6a4789a4
Tom Webb <tomwebb1992@gmail.com>
2024-03-20 13:05:18 +0000

    added playsinline to video. Issues with playing on iOS

--- project360/src/components/BackgroundVideo.jsx
+++ project360/src/components/BackgroundVideo.jsx
@@ -4,7 +4,7 @@ function BackgroundVideo() {
   return (
     <div className="background-image">
       <h1 className="title">ATEDO 360 </h1>
-      <video autoPlay muted>
+      <video playsinline autoPlay muted>
         <source src={dance} type="video/mp4" />
       </video>
     </div>
