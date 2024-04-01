9955aec501b2e2f5b0c8fd93928f5f2602a86623
Sara Thampi <saraannthampi@gmail.com>
2024-04-01 14:42:15 +0100

    background added

---
+++ project360/src/data/dummyData.js
@@ -0,0 +1,33 @@
+const dummyData = [ 
+    {
+    id: 1,
+    videoName: "Nao Homies Party Viernes",
+    date: "23-09-01",
+    videoUrl: "https://giphy.com/embed/U3Djoub6n7HBswsCBx",
+    downloadUrl: "https://drive.google.com/drive/folders/1PkvAg4id-A3r83YwFw9hldN0YL_epbEC",
+  },
+  {
+    id: 2,
+    videoName: "Nao Gordo Reve Night",
+    date: "23-08-31",
+    videoUrl: "https://giphy.com/embed/iDBSSI7bRq0tq",
+    // videoUrl: "https://drive.google.com/file/d/1xN5qUYSawPdNBwpOsCq0UF44EYOkq1y5/view?usp=drive_link",
+    downloadUrl: "https://drive.google.com/drive/folders/1PkvAg4id-A3r83YwFw9hldN0YL_epbEC",
+  },
+  {
+    id: 3,
+    videoName: "Nao Guilty Lunes",
+    date: "23-08-29",
+    videoUrl: "https://giphy.com/embed/kp0n1mfC73XEc",
+    downloadUrl: "https://drive.google.com/drive/folders/1PkvAg4id-A3r83YwFw9hldN0YL_epbEC",
+  },
+  {
+    id: 4,
+    videoName: "Nao Guilty Lunes",
+    date: "23-08-28",
+    videoUrl: "https://giphy.com/embed/ano4sUdpQc0Ax6WtEM" ,
+    downloadUrl: "https://drive.google.com/drive/folders/1PkvAg4id-A3r83YwFw9hldN0YL_epbEC",
+  }
+  ]
+
+  export default dummyData;
