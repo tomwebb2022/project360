5e3bc30110b55e1fe7e2122600b6cbd9ee1e820d
Sara Thampi <saraannthampi@gmail.com>
2024-04-01 17:32:13 +0100

    axios fetch video data

--- project360/src/pages/Gallery.jsx
+++ project360/src/pages/Gallery.jsx
@@ -2,11 +2,29 @@ import VideoCard from "../components/gallery/VideoCard"
 import { Link } from "react-router-dom"
 import HeroGallery  from "../components/gallery/HeroGallery"
 import dummyData from "../data/dummyData"
+import axios from "axios"
+import { useEffect, useState } from "react"
 import "./Gallery.css"
 
-
-
 const Gallery = () => {
+  const [videoData, setVideoData] = useState([]);
+
+  useEffect(() => {
+    async function getVideos() {
+      try {
+        const videoData = await axios.get("http://localhost:3000/gallery")
+        console.log(videoData.data);
+
+        setVideoData(videoData.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getVideos();
+    
+  }, []);
+  
   return (
     <div className="gallery-page">
       <HeroGallery />
@@ -30,9 +48,3 @@ const Gallery = () => {
 
 export default Gallery
 
-/*
-<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/U3Djoub6n7HBswsCBx" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/e4-cgd-celebsgodating-U3Djoub6n7HBswsCBx">via GIPHY</a></p>
-<iframe src="https://giphy.com/embed/iDBSSI7bRq0tq" width="480" height="384" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/sexy-man-iDBSSI7bRq0tq">via GIPHY</a></p>
-<iframe src="https://giphy.com/embed/kp0n1mfC73XEc" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/sexy-girl-kp0n1mfC73XEc">via GIPHY</a></p>
-<iframe src="https://giphy.com/embed/ano4sUdpQc0Ax6WtEM" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/Yandy-com-model-yandy-yandyswim-ano4sUdpQc0Ax6WtEM">via GIPHY</a></p>
- */
