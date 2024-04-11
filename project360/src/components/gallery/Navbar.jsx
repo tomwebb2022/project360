b815087b59e3942dbaaa7c23d712ec9d6d7f4c4f
Sara Thampi <saraannthampi@gmail.com>
2024-04-11 21:25:17 +0100

    add / route to display success message when connected to server

--- project360/src/components/gallery/Navbar.jsx
+++ project360/src/components/gallery/Navbar.jsx
@@ -10,13 +10,13 @@ export default function Navbar() {
             <a href="/">
                 <img className="nav-logo" src={logo2} alt="logo" />
             </a>
-            {console.log(window.innerWidth)}
+            {/* {console.log(window.innerWidth)} */}
 
-            {window.innerWidth > 1000 ? (
-                <img className="nav-brand" src={logo} alt="logo" />
-            ) : (
-                <img className="nav-brand" src={logosm} alt="logo" />
-            )}
+          
+            <img className="nav-brand" src={logo} alt="logo" />
+          
+            <img className="nav-brandsm" src={logosm} alt="logo" />
+        
 
             <Link to="/">
                 <h2>Home</h2>
--- server/index.js
+++ server/index.js
@@ -32,3 +32,6 @@ app.listen(PORT, () => {
 app.use("/emails", emailRouter);
 app.use("/users", userRouter);
 app.use("/gallery", galleryRouter);
+app.use("/", (req, res) => {
+  res.send("Connected successfully to Project360 server! Use routes /emails, /users, or /gallery to access data.");
+});
