eead416dd7a9f0ada88176bf1218a0ea79047cb2
Sara Thampi <saraannthampi@gmail.com>
2024-04-23 22:01:16 +0100

    import path

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import path from "path";
 import cors from "cors";
 import dotenv from "dotenv";
 import { emailRouter } from "./routes/routes.js";
