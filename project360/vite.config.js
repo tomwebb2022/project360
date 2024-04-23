debf82ae3af5efd7a0f18baceaa274496e985bc7
Sara Thampi <saraannthampi@gmail.com>
2024-04-23 21:43:46 +0100

    build again

--- project360/vite.config.js
+++ project360/vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './src',
+  root: process.cwd(),
   build: {
     outDir: './dist',
     assetsDir: './assets',
+    base: '/',
   },
 })
