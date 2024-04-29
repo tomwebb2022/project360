4b293b0f7260a386cd4c350f0a8a6b27b8ce14b6
Sara Thampi <saraannthampi@gmail.com>
2024-04-29 09:55:20 +0100

    vercel json

--- project360/src/components/gallery/VideoCard.jsx
+++ project360/src/components/gallery/VideoCard.jsx
@@ -18,7 +18,7 @@ export default function VideoCard(props) {
         <div className="gallery-card">
             <div className="galleryVidWrapper">
                 <a href={downloadUrl} target="_blank" >
-                <iframe className="galleryVid" src={videoUrl} width="100%" height="100%" scrolling="no" align="center" ></iframe> </a>
+                <iframe className="galleryVid" src={videoUrl} width="100%" height="100%" scrolling="no" align="center"  >Your browser does not support iframes</iframe> </a>
             </div>
             <a href={downloadUrl} target="_blank" ><p>{date} {videoName}</p></a>
             
